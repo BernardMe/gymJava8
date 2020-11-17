b62dd109c016ba0553d1e28e737fb642bb2bca66
wangzhuo <wangzhuo@967111.com>
2020-11-17 17:00:11 +0800

    add 根据开始时间和结束时间返回时间段内的时间字符串集合

--- src/main/java/org/learnit/date/TestDate01.java
+++ src/main/java/org/learnit/date/TestDate01.java
@@ -14,6 +14,18 @@ import java.util.List;
  */
 public class TestDate01 {
 
+    /** 日期格式 **/
+    public interface DATE_PATTERN {
+        String HHMMSS = "HHmmss";
+        String HH_MM_SS = "HH:mm:ss";
+        String YYYYMMDD = "yyyyMMdd";
+        String YYYY_MM_DD = "yyyy-MM-dd";
+        String YYYYMMDDHHMMSS = "yyyyMMddHHmmss";
+        String YYYYMMDDHHMMSSSSS = "yyyyMMddHHmmssSSS";
+        String YYYY_MM_DD_HH_MM_SS = "yyyy-MM-dd HH:mm:ss";
+        String YYYY_MM_DD_HH_MM_SS_SSS = "yyyy-MM-dd HH:mm:ss.SSS";
+    }
+
     public static void main(String[] args) throws Exception {
         String start = "2014-01-03";
         String end = "2014-03-05";
@@ -47,10 +59,39 @@ public class TestDate01 {
             if (endDate.after(cal.getTime())) {
                 lDate.add(cal.getTime());
             } else {
-                break;
+                bContinue = false;
             }
         }
         lDate.add(endDate);// 把结束时间加入集合
         return lDate;
+    }
+
+    /**
+     * 根据开始时间和结束时间返回时间段内的时间字符串集合
+     *
+     * @param beginDate
+     * @param endDate
+     * @return List
+     */
+    public static List<String> getDateStrListBetweenTwoDate(Date beginDate, Date endDate, String pattern) {
+        SimpleDateFormat sdf = new SimpleDateFormat(pattern);
+        List<String> lDateStr = new ArrayList<>();
+        lDateStr.add(sdf.format(beginDate));// 把开始时间加入集合
+        Calendar cal = Calendar.getInstance();
+        // 使用给定的 Date 设置此 Calendar 的时间
+        cal.setTime(beginDate);
+        boolean bContinue = true;
+        while (bContinue) {
+            // 根据日历的规则，为给定的日历字段添加或减去指定的时间量
+            cal.add(Calendar.DAY_OF_MONTH, 1);
+            // 测试此日期是否在指定日期之后
+            if (endDate.after(cal.getTime())) {
+                lDateStr.add(sdf.format(cal.getTime()));
+            } else {
+                bContinue = false;
+            }
+        }
+        lDateStr.add(sdf.format(endDate));// 把结束时间加入集合
+        return lDateStr;
     }
 }
