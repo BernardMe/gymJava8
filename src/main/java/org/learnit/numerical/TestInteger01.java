31f2ff4dc5f749588ef423e7b344a45d6e72d653
wangzhuo <mt_cook@126.com>
2021-11-26 20:14:17 +0800

    add Integer类型计算测试01

---
+++ src/main/java/org/learnit/numerical/TestInteger01.java
@@ -0,0 +1,23 @@
+package org.learnit.numerical;
+
+/**
+ * Integer类型计算测试01
+ *
+ * @author wangzhuo
+ */
+public class TestInteger01 {
+
+    public static void main(String[] args) {
+
+        Integer a = new Integer("50");
+        Integer b = new Integer("10");
+        Integer c = new Integer("20");
+
+        // 一次减法计算
+        System.out.println(String.format("一次减法计算: %s", a - b));
+        // 两次次减法计算
+        System.out.println(String.format("两次次减法计算: %s", a - b - c));
+        // 多次次减法计算
+        System.out.println(String.format("两次次减法计算: %s", a - (b - c)));
+    }
+}
